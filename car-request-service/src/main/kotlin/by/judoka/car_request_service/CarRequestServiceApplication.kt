19e1d36b60f6bccaae43a92945934211066c9330
borecjudo2 <borecjudo2@gmail.com>
2024-10-12 17:51:52 +0300

    Init commit

---
+++ car-request-service/src/main/kotlin/by/judoka/car_request_service/CarRequestServiceApplication.kt
@@ -0,0 +1,11 @@
+package by.judoka.car_request_service
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CarRequestServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<CarRequestServiceApplication>(*args)
+}
---
+++ car-request-service/src/test/kotlin/by/judoka/car_request_service/CarRequestServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package by.judoka.car_request_service
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CarRequestServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
